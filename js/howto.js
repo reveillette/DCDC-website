50545573c5556e855f2157317a4c90483250e143
reveillette <reveillette@gmail.com>
2015-01-27 21:25:19 -0500

    updated files

--- js/howto.js
+++ js/howto.js
@@ -1,17 +1,49 @@
+// Title slider
+
+var howtos = [
+	"Start a Social Media Marketing Strategy?",
+	"Organize a Neighborhood Cleanup Day?",
+	"Prepare a Grant Proposal?",
+	"Outreach to My Community?"
+]; 
+
 $(document).ready(function() { 
-	$("h2").slideUp(400);
-	$("#flip").text("Prepare a Grant Proposal?");
-	$("h2").slideDown(1000);
-	$("h2").slideUp(1000);
-	$("#flip").text("Start a Social Media Marketing Strategy?");
-	$("h2").slideDown(1000);
+	for (var i=0; i< howtos.length; i++) {
+		$("h2").fadeToggle(1000);
+		$("#flip").text(howtos[i]);
+		$("h2").fadeToggle(1000);
+		console.log(howtos[i]);
+		console.log(i);
+	}
+});	
 
-	$(window).onScroll(function() { 
-		$("#main").css("position", "absolute");
-		$("#main").css("top", 0);
+
+// Login window
+$(document).ready(function() { 
+	$("#login-button").click(function() {
+		$("#screen").fadeIn();
+		$("#login-window").fadeIn();
+	});
+
+	$(".close-button").click(function() {
+		$("#screen").fadeOut();
+		$("#login-window").fadeOut();
+	});
 });
 
-});
+
+// // Main screen scroll to place
+
+// function scrolljump(id){
+//     var divid = document.getElementById(id);
+//     divid.scrollIntoView({behavior: "smooth"});
+//     return false;
+// }
+
+// $(window).scroll(function() { 
+// 	scrolljump("main");
+// });
+
 
 
 //		$(".title>h2").slideToggle(1000);
