981b4078e657e6723fb34a462dcf9ac0d0d5361e
reveillette <reveillette@gmail.com>
2015-01-30 06:24:50 -0500

    updated files

--- js/howto.js
+++ js/howto.js
@@ -1,226 +1,290 @@
+// Load header and footer
+	$(document).on('ready', function(event) {
+		$("#header").load('../html/header.html');
+		$("#footer").load('../html/footer.html');
+	});
+
+	function addCurrentClass() {
+		var shortURL = document.URL.split("/");
+		console.log(shortURL);
+		var currentPage = shortURL[3].replace(".html", "");
+		var id = "header-" + currentPage + "-url";
+		$("#" + id).toggleClass("current-link");
+	};
+
+	function addCopyrightYear() {
+		var currentYear = new Date().getFullYear();
+		$("#current-year").text(currentYear);
+	};
+
+// // Main screen jump to place
+
+	// function scrollJump(id) {
+	//     var divid = document.getElementById(id);
+	//     divid.scrollIntoView({behavior: "smooth"});
+	//     return false;
+	// }
+
+	// $(window).scroll(function() { 
+	// 	scrolljump("main");
+	// });
+
+
 // Title slider
-		var howtos = [
-			"Start a Social Media Marketing Strategy?",
-			"Organize a Neighborhood Cleanup Day?",
-			"Prepare a Grant Proposal?",
-			"Outreach to My Community?"
-		]; 
+	var howtos = [
+		"Start a Social Media Marketing Strategy?",
+		"Organize a Neighborhood Cleanup Day?",
+		"Prepare a Grant Proposal?",
+		"Outreach to My Community?",
+		"Create a Community How-to-Guide?"
+	]; 
 
-		function fadeTitle(title) {
-
-		};
-
-		$(document).on('ready', function() { 
-				event.preventDefault();
-					for (var i=0; i< howtos.length; i++) {
-						var text = howtos[i];
-						console.log(text);
-						console.log(i);			
-					$("#flip").fadeToggle(3000);
+	$(document).on('ready', function(event) {
+		$("#flip").text(howtos[0]);
+		for (var i=1; i< howtos.length; i++) {
+			var text = howtos[i];
+			$("#flip").fadeOut(300, function(text, i) {
+				return function(){
 					$("#flip").text(text);
-					};
-		});	
+					if (i == howtos.length - 1) {
+						$("#flip").css("color", "#ffcc33");	
+					}
+				}
+			}(text, i));
+			$("#flip").fadeIn(800);
+		};
+	});	
+
 
 // Overlays
 
-		function addScreen() { 
-			$("body").append("<div id='screen'></div>");	
-		};
+	function addScreen() { 
+		$("body").append("<div id='screen'></div>");	
+	};
 
-		function addLoginWindow() {
-			$("body").append("<div class='overlay' id='login-window'><img class='close-button' src='assets/close.svg'><h5>Login</h5><a id='register-link' href='#'><strong>Create an account</strong></a><form action='action_page.php'><input type='email' name='email' placeholder='Email'><input type='password' name='password' placeholder='Password'><br><br><br><input type='submit' name='Sign in' value='Sign in'></form><button class='social-button' id='google'></button><button class='social-button' id='twitter'></button></div>");
-		}
+	function addLoginWindow() {
+		$("body").append("<div class='overlay' id='login-window'></div>");
+		$("#login-window").load('../html/login.html');
+	}
 
-		function addDownloadWindow(filename) {
-			$("body").append("<div class='overlay' id='download-window'><img class='close-button' src='assets/close.svg'><h5>Download</h5></a><form name='download-form'><input type='text' name='name' placeholder='Name or Organization'><input type='email' name='email' placeholder='Email'><input type='text' name='zip' placeholder='Zip Code'><br><br><br><input type='submit' id='download-now' name='download' value='Download'></form></div>");
-		}
+	function addDownloadWindow(filename) {
+		$("body").append("<div class='overlay' id='download-window'></div>");
+		$("#download-window").load('../html/download.html');
+	}
 
-		function addRegisterWindow() {
-			$("body").append("<div class='overlay' id='register-window'><img class='close-button' src='assets/close.svg'><form action='action_page.php'><div><h5>Register</h5><input type='text' name='name' placeholder='Name or Organization'><input type='email' name='email' placeholder='Email'><input type='password' name='password' placeholder='Password'><br><br><br></div><div><h5>We'd love to know more</h5><input type='text' name='title' placeholder='Title'><input type='text' name='role' placeholder='Role'><input type='text' name='zip' placeholder='Zip Code'><br><br><br></div><button class='social-button' id='google'></button><button class='social-button' id='twitter'></button><input type='submit' name='Sign in' value='Submit'></form></div>");
-		}
+	function addRegisterWindow() {
+		$("body").append("<div class='overlay' id='register-window'></div>");
+		$("#register-window").load('../html/register.html');
+	}
 
-		function addShareWindow() {
-			$("body").append("<div class='overlay' id='share-window'><img class='close-button' src='assets/close.svg'><form action='action_page.php'><div><h5>Get the link</h5><input readonly type='text' name='link' placeholder='http://bit.ly/3jdl98'><input type='submit' id='copy-link' name='copy' value='Copy'></div><div><h5>Share</h5><a href='http://www.twitter.com'><img src='assets/icons/twitter.png'></a><a href='https://plus.google.com'><img src='assets/icons/google.png'></a><a href='http://www.tumblr.com'><img src='assets/icons/tumblr.png'></a><a href='http://www.facebook.com'><img src='assets/icons/facebook.png'></a></div><div><h5>Send an email</h5><input type='email' name='email' placeholder='Enter an email address'><input type='submit' name='send' value='Send'></form></div></div>");
-		}
-		function addSearchWindow(filename) {
-			$("body").append("<div class='overlay' id='search-window'><img class='close-button' src='assets/close.svg'><h5>Search for a guide</h5></a><form name='search-form'><input type='search' name='search' placeholder='Search'><br><br><br><input type='submit' id='search-button' name='search' value='Search'></form></div>");
-		}
+	function addShareWindow() {
+		$("body").append("<div class='overlay' id='share-window'></div>");			
+		$("#share-window").load('../html/share.html');
+	}
 
-		function showOverlay(trigger, overlay, functionName) {			
-			$(document).on('click', trigger, function(event) {
-				event.preventDefault();
-				if ($("#screen").length != 0) {
-					$("#screen").fadeIn();
-				} else {
-					addScreen();
-					$("#screen").fadeIn();
-				};	
-				if ($(overlay).length != 0) {
-					$(overlay).fadeIn();
-				} else {
-					functionName();
-					$(overlay).fadeIn();
-				};
-			});
-		}
-			
-		// Close button 
-		$(document).on('click', '.close-button', function(event) {
-		    event.preventDefault();
-			$("#screen").fadeOut();
-			$(".overlay").fadeOut();
-		});
+	function addSearchWindow(filename) {
+		$("body").append("<div class='overlay' id='search-window'></div>");
+		$("#search-window").load('../html/search.html');
+	}
 
-		$(document).on('click', '#screen', function(event) {
+	function addTermsWindow() {
+		$("body").append("<div class='overlay' id='terms-window'><img class='close-button' src='assets/close.svg'><h5>Terms and Conditions</h5><div id='terms-text'></div></div>");
+		$("#terms-text").load('../html/terms.html');
+		$("#terms-window").append(function() {
+			return "<br><br><br><p>Last Updated " + lastModified('../html/terms.html') + "</p>";
+		}); 
+	}
+
+	function addPrivacyWindow() {
+		$("body").append("<div class='overlay' id='privacy-window'><img class='close-button' src='assets/close.svg'><h5>Privacy Policy</h5><div id='privacy-text'></div></div>");
+		$("#privacy-text").load('../html/privacy.html');
+		$("#privacy-window").append(function() {
+			return "<br><br><br><p>Last Updated " + lastModified('../html/privacy.html') + "</p>";
+		}); 
+	}
+
+	function addContactWindow() {
+		$("body").append("<div class='overlay' id='contact-window'></div>");
+		$("#contact-window").load('../html/contact.html');
+	}
+
+
+	function showOverlay(trigger, overlay, functionName) {			
+		$(document).on('click', trigger, function(event) {
 			event.preventDefault();
-			$("#screen").fadeOut();
-			$(".overlay").fadeOut();
+			if ($("#screen").length != 0) {
+				$("#screen").fadeIn();
+			} else {
+				addScreen();
+				$("#screen").fadeIn();
+			};	
+			if ($(overlay).length != 0) {
+				$(overlay).fadeIn();
+			} else {
+				functionName();
+				$(overlay).fadeIn();
+			};
+			$("body").css("overflow", "hidden");
 		});
-
-		// Login overlay
-		showOverlay("#login-button", "#login-window", addLoginWindow);
-			
-		// Register overlay 
-		showOverlay("#register-button-top", "#register-window", addRegisterWindow);
-		showOverlay("#register-link", "#register-window", addRegisterWindow);
-		showOverlay("#register-button", "#register-window", addRegisterWindow);
-
-
-		// Register overlay 
-		showOverlay("#search", "#search-window", addSearchWindow);
-
+	}
 		
-// Form validation
-function validateForm(formName) {
-	var email = document.forms[formName]["email"].value; 
-    var name = document.forms[formName]["name"].value;
-    var zip = document.forms[formName]["zip"].value;
-    var title = document.forms[formName]["title"].value;
-    var role = document.forms[formName]["role"].value;
-    var password = document.forms[formName]["password"].value;
+	// Close button 
+	$(document).on('click', '.close-button', function(event) {
+	    event.preventDefault();
+		$("#screen").fadeOut();
+		$(".overlay").fadeOut();
+		$("body").css("overflow", "visible");	
+	});
 
-    return false;
-    // if (name == null || name == "") {
-    //     alert("Please provide your full name");
-    //     return false;
-    // }
-}
+	$(document).on('click', '#screen', function(event) {
+		event.preventDefault();
+		$("#screen").fadeOut();
+		$(".overlay").fadeOut();
+		$("body").css("overflow", "visible");	
+	});
+
+	// Login overlay
+	showOverlay("#login-button", "#login-window", addLoginWindow);
+		
+	// Register overlay 
+	showOverlay("#register-link", "#register-window", addRegisterWindow);
+	showOverlay("#register-button", "#register-window", addRegisterWindow);
+
+
+	// Register overlay 
+	showOverlay("#search", "#search-window", addSearchWindow);
+
+	// Terms overlay
+	showOverlay("#terms", "#terms-window", addTermsWindow);
+
+	// Privacy overlay
+	showOverlay("#privacy", "#privacy-window", addPrivacyWindow);
+
+	// Contact overlay
+	showOverlay("#contact", "#contact-window", addContactWindow);
+
 
 // Library buttons (like, download, share)
+	// Likes
+    $(document).on('click', '.likes', function(event) {
+        event.preventDefault();
+        $(this).toggleClass("likes").toggleClass("isliked");
+        var i = $(this).text();
+        i++;
+        $(this).text(i);
+    });
+
+    $(document).on('click', '.isliked', function(event) {
+        event.preventDefault();
+        $(this).toggleClass("isliked").toggleClass("likes");
+        var j = $(this).text();
+        j--;
+        $(this).text(j);
+    });
+
+	// Downloads
+	// function downloadFile(numDownloads) {
+	// 	if (validateForm()) {
+	// 		$("#download-now").on('click', "#download-window", function(event) {
+	// 			event.preventDefault();
+	// 			numDownloads++;
+	// 			window.open("guides/file.pdf", 'download_window');	
+	// 		});	
+	// 	}
+	// };	
+
+	$(document).on('click', ".downloads", function(event) {
+		var numDownloads = $(this).text();
+		event.preventDefault();
+		if ($("#screen").length != 0) {
+			$("#screen").fadeIn();
+		} else {
+			addScreen();
+			$("#screen").fadeIn();
+		};	
+		if ($("#download-window").length != 0) {
+			$("#download-window").fadeIn();
+		} else {
+			addDownloadWindow();
+			$("#download-window").fadeIn();
+		};
+
+		if (validateForm()) {
+			// downloadFile();
+		} else {
+			alert("Please check your information and try again.");
+		};
+
+		$(this).text(numDownloads);		
+	});
+
+	//Shares 
+	$(document).on('click', ".shares", function(event) {
+		var numShares = $(this).text();
+		event.preventDefault();
+		if ($("#screen").length != 0) {
+			$("#screen").fadeIn();
+		} else {
+			addScreen();
+			$("#screen").fadeIn();
+		};	
+		if ($("#share-window").length != 0) {
+			$("#share-window").fadeIn();
+		} else {
+			addShareWindow();
+			$("#share-window").fadeIn();
+		};
+
+		if (validateForm()) {
+			// shareFile();
+		} else {
+			alert("Please check your information and try again.");
+		};
+
+		$(this).text(numDownloads);		
+	});
+
+	$(".shares").click(function() {
+	    $(this).toggleClass("likes").toggleClass("isliked");
+	    var i = $(this).text();
+	    i++;
+	    $(this).text(i);
+	});
+
+	// function shareFile(numShares) {
+	// 	if (validateForm()) {
+	// 		$("#share-now").on('click', "#download-window", function(event) {
+	// 			event.preventDefault();
+	// 			numDownloads++;
+	// 			window.open("guides/file.pdf", 'download_window');	
+	// 		});	
+	// 	}
+	// };	
 
 
-		// // Likes
-		// $(".likes").click(function() {
-		//     $(this).toggleClass("likes").toggleClass("isliked");
-		//     var i = $(this).text();
-		//     i++;
-		//     $(this).text(i);
-		// });
+// Miscellaneous functions
 
-		// $(".isliked").click(function() {
-		//     $(this).toggleClass("isliked").toggleClass("likes");
-		//     var j = $(this).text();
-		//     j--;
-		//     console.log(j);
-		//     $(this).text(j);
-		// });
+	// Form validation
+	function validateForm(formName) {
+		var email = document.forms[formName]["email"].value; 
+	    var name = document.forms[formName]["name"].value;
+	    var zip = document.forms[formName]["zip"].value;
+	    var title = document.forms[formName]["title"].value;
+	    var role = document.forms[formName]["role"].value;
+	    var password = document.forms[formName]["password"].value;
 
-		// Downloads
-		// function downloadFile(numDownloads) {
-		// 	if (validateForm()) {
-		// 		$("#download-now").on('click', "#download-window", function(event) {
-		// 			event.preventDefault();
-		// 			numDownloads++;
-		// 			window.open("guides/file.pdf", 'download_window');	
-		// 		});	
-		// 	}
-		// };	
+	    return false;
+	    // if (name == null || name == "") {
+	    //     alert("Please provide your full name");
+	    //     return false;
+	    // }
+	}
 
-		$(document).on('click', ".downloads", function(event) {
-			var numDownloads = $(this).text();
-			event.preventDefault();
-			if ($("#screen").length != 0) {
-				$("#screen").fadeIn();
-			} else {
-				addScreen();
-				$("#screen").fadeIn();
-			};	
-			if ($("#download-window").length != 0) {
-				$("#download-window").fadeIn();
-			} else {
-				addDownloadWindow();
-				$("#download-window").fadeIn();
-			};
-
-			if (validateForm()) {
-				// downloadFile();
-			} else {
-				alert("Please check your information and try again.");
-			};
-
-			$(this).text(numDownloads);		
-		});
-
-		//Shares 
-		$(document).on('click', ".shares", function(event) {
-			var numShares = $(this).text();
-			event.preventDefault();
-			if ($("#screen").length != 0) {
-				$("#screen").fadeIn();
-			} else {
-				addScreen();
-				$("#screen").fadeIn();
-			};	
-			if ($("#share-window").length != 0) {
-				$("#share-window").fadeIn();
-			} else {
-				addShareWindow();
-				$("#share-window").fadeIn();
-			};
-
-			if (validateForm()) {
-				// shareFile();
-			} else {
-				alert("Please check your information and try again.");
-			};
-
-			$(this).text(numDownloads);		
-		});
-
-		$(".shares").click(function() {
-		    $(this).toggleClass("likes").toggleClass("isliked");
-		    var i = $(this).text();
-		    i++;
-		    $(this).text(i);
-		});
-
-		// function shareFile(numShares) {
-		// 	if (validateForm()) {
-		// 		$("#share-now").on('click', "#download-window", function(event) {
-		// 			event.preventDefault();
-		// 			numDownloads++;
-		// 			window.open("guides/file.pdf", 'download_window');	
-		// 		});	
-		// 	}
-		// };	
-
-// // Main screen scroll to place
-
-// function scrolljump(id){
-//     var divid = document.getElementById(id);
-//     divid.scrollIntoView({behavior: "smooth"});
-//     return false;
-// }
-
-// $(window).scroll(function() { 
-// 	scrolljump("main");
-// });
-
-
-
-//		$(".title>h2").slideToggle(1000);
-//		$("#flip").text("Start a Social Media Marketing Strategy?");
-//		$(".title>h2").slideToggle(1000);
-//		$("#flip").text("Find Resources for My Small Business?");
-//		$(".title>h2").slideToggle(1000);	
-
+	// Get last modified date of file on server
+	function lastModified(url) {
+	    var req = new XMLHttpRequest();
+	    req.open("GET", url, false);
+	    req.send(null);
+		var dateLastMod = req.getResponseHeader("Last-Modified");
+		return dateLastMod
+	}
